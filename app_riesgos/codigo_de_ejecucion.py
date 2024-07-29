9e81e30c718cf2678a4cab2d6cf70a51990ccfa5
IvanAGzgz <161027682+IvanAGzgz@users.noreply.github.com>
2024-07-29 02:13:20 +0200

    Update codigo_de_ejecucion.py

--- app_riesgos/codigo_de_ejecucion.py
+++ app_riesgos/codigo_de_ejecucion.py
@@ -1,4 +1,4 @@
-import cloudpickle
+import pickle
 import pandas as pd
 
 import warnings
@@ -9,7 +9,7 @@ def ejecutar_modelos(df):
     #6.CARGA PIPE DE EJECUCION
 
     with open('pipe_ejecucion.pickle', mode='rb') as file:
-       pipe_ejecucion = cloudpickle.load(file)
+       pipe_ejecucion = pickle.load(file)
 
     #7.EJECUCION
     scoring = pipe_ejecucion.predict_proba(df)[:, 1]

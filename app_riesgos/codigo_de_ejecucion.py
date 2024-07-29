eedc937325d0631db4846ae32a3f114254e56d94
IvanAGzgz <161027682+IvanAGzgz@users.noreply.github.com>
2024-07-29 02:09:40 +0200

    Update codigo_de_ejecucion.py

--- app_riesgos/codigo_de_ejecucion.py
+++ app_riesgos/codigo_de_ejecucion.py
@@ -8,13 +8,7 @@ def ejecutar_modelos(df):
 
     #6.CARGA PIPE DE EJECUCION
 
-    ruta_proyecto = r'C:\Users\iagzg\Desktop\D4FB\D4FB\EstructuraDirectorio\03_MACHINE_LEARNING\06-MACHINE LEARNING PROJECT FRAMEWORK\CASOS\04_BANK\\'
-
-    nombre_pipe_ejecucion = 'pipe_ejecucion.pickle'
-
-    ruta_pipe_ejecucion = ruta_proyecto + '/04_Modelos/' + nombre_pipe_ejecucion
-
-    with open(ruta_pipe_ejecucion, mode='rb') as file:
+    with open('pipe_ejecucion.pickle', mode='rb') as file:
        pipe_ejecucion = cloudpickle.load(file)
 
     #7.EJECUCION
